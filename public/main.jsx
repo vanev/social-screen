164d7a4035c49d660acdba0bf169c30e86c4e2de
Evan Siegel <evan@harrys.com>
2015-02-27 14:34:10 -0500

    8 Images

--- public/main.jsx
+++ public/main.jsx
@@ -1,13 +1,22 @@
 var socket = io();
 
 var Images = React.createClass({
+    MAX_COUNT: 8,
+
     getInitialState: function () {
         return { images: this.props.images };
     },
 
     componentDidMount: function () {
         socket.on('new_images', function (images_data) {
-            this.setState({ images: images_data });
+            var images = _.reduce(images_data, function (memo,image_data) {
+                console.log(memo);
+                if (!_.contains(memo, image_data)) memo.push(image_data);
+                while (memo.length > this.MAX_COUNT) { memo.shift(); }
+                return memo;
+            }.bind(this), this.state.images);
+
+            this.setState({ images: images });
         }.bind(this));
     },
 
